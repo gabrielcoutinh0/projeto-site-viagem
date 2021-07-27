f31e0e1166911c73d3f0c94060c2334b5befb875
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2021-07-27 01:14:08 -0300

    Criando um smoothScroll.

--- js/index.js
+++ js/index.js
@@ -34,12 +34,12 @@ function eventDisable(element){
     });
 }
 
-function screenUp(){
+/*function screenUp(){
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
-}
+}*/
 
 function buttonScroll(){
     if(window.scrollY <= 200){
@@ -50,3 +50,46 @@ function buttonScroll(){
 }
 
 window.addEventListener('scroll', buttonScroll);
+
+function smoothScroll(target, duration){
+    target = document.querySelector(target);
+    let targetPosition = target.getBoundingClientRect().top;
+    let startPostion = window.pageYOffset;
+    let distance = targetPosition - startPostion;
+    let startTime = null;
+
+    function animation(currentTime){
+        if(startTime === null) startTime = currentTime;
+        let timeElapsed = currentTime - startTime;
+        let run = ease(timeElapsed, startPostion, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+        console.log('timeElapsed: ' + timeElapsed + 'duration: ' + duration);
+    }
+
+    function ease(time, start, distance, duration){
+        time /= duration / 2;
+        if(time < 1) return distance / 2 * time * time + start;
+        time--;
+        return -distance / 2 * (time * (time - 2) -1) + start;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+let destinations = document.querySelector('#a-destinations');
+let prices = document.querySelector('#a-prices');
+let scrollbutton = document.querySelector('.scrollbutton');
+
+
+destinations.addEventListener('click', () => {
+    smoothScroll('#destinations', 1000);
+});
+
+prices.addEventListener('click', () => {
+    smoothScroll('#prices', 1000);
+});
+
+scrollbutton.addEventListener('click', () => {
+    smoothScroll('.logo', 1000);
+});
